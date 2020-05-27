4516ef09ebc123762aebc7a84d069d8aec16dad6
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-05-27 18:46:03 +0900

    5_27_기본틀(씬추가), 플레이어 조이스틱구현

---
+++ STAC/Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public void Left()
+    {
+        
+    }
+
+    public void Right()
+    {
+        
+    }
+}
