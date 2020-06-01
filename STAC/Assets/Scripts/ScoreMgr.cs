d02ff4fb6b37710a27254a740d292daa6954ce36
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-06-01 20:08:56 +0900

    6_1_캐릭터선택 제작중

---
+++ STAC/Assets/Scripts/ScoreMgr.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMgr : MonoBehaviour
+{
+    public static ScoreMgr instance;
+    private string highscoreKey = "highscore";
+    
+    public int highScore = 0;
+    public int score = 0;
+    public bool isHighScore = false;
+    void Start()
+    {
+        //싱글톤
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        highScore = PlayerPrefs.GetInt(highscoreKey, 0); //저장된 값 받아옴
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void GameStart() //게임 시작 시 초기화
+    {
+        score = 0;
+        isHighScore = false;
+    }
+    
+    public void AddScore(int v)
+    {
+        score += v;
+        if (score > highScore) //최고점수 갱신
+        {
+            highScore = score;
+            PlayerPrefs.SetInt(highscoreKey,highScore);
+            isHighScore = true;
+        }
+    }
+}
