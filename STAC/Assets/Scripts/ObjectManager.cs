ff9c6acbe95efd888c7892a596ad67af666a3574
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-29 19:37:40 +0900

    7_29_구글스프레드시트연동

--- STAC/Assets/Scripts/ObjectManager.cs
+++ STAC/Assets/Scripts/ObjectManager.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
+
    public int bulletNum;
 
    //프리팹 할당
