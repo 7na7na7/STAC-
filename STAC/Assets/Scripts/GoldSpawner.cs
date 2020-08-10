6528a107e63b6eea456527be770a6852f2873dac
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-08-10 19:18:20 +0900

    8_10_골드스포너 시간지날수록 골드생성쿨타임 짧아지도록

--- STAC/Assets/Scripts/GoldSpawner.cs
+++ STAC/Assets/Scripts/GoldSpawner.cs
@@ -6,8 +6,11 @@ using Random = UnityEngine.Random;
 
 public class GoldSpawner : MonoBehaviour
 {
+    public float minGoldTime; //골드가 나타나는 최소 빈도
+    public float timeMinusValue; 
+    public float timeMinusDelay;
     public static GoldSpawner instance;
-    public float minDelay, maxDelay;
+    public float delay;
     private float radMaxX, radMinY, radMinX,radMaxY;
     public Transform player;
 
@@ -19,18 +22,18 @@ public class GoldSpawner : MonoBehaviour
     private void Start()
     {
         StartCoroutine(spawn());
-
-        radMaxX = FindObjectOfType<Spawner>().radMaxX*Camera.main.orthographicSize;
-        radMinY = FindObjectOfType<Spawner>().radMinY*Camera.main.orthographicSize;
-        radMinX = FindObjectOfType<Spawner>().radMinX*Camera.main.orthographicSize; 
-        radMaxY = FindObjectOfType<Spawner>().radMaxY*Camera.main.orthographicSize;
+        StartCoroutine(goldTimeMinus());
+        radMaxX = FindObjectOfType<Spawner>().radMaxX;
+        radMinY = FindObjectOfType<Spawner>().radMinY;
+        radMinX = FindObjectOfType<Spawner>().radMinX;
+        radMaxY = FindObjectOfType<Spawner>().radMaxY;
     }
 
     IEnumerator spawn()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(minDelay,maxDelay));
+            yield return new WaitForSeconds(delay);
             if (player != null)
             {
                 GameObject gold=ObjectManager.instance.MakeObj(102);
@@ -38,29 +41,41 @@ public class GoldSpawner : MonoBehaviour
                 if (r == 0||r==1) //위
                 {
                     gold.transform.position = new Vector2(
-                        Random.Range(player.position.x - radMaxX, player.position.x + radMaxX),
-                        Random.Range(player.position.y + radMinY, player.position.y + radMaxY));
+                        Random.Range(player.position.x - radMaxX*Camera.main.orthographicSize, player.position.x + radMaxX*Camera.main.orthographicSize),
+                        Random.Range(player.position.y + radMinY*Camera.main.orthographicSize, player.position.y + radMaxY*Camera.main.orthographicSize));
                 }
                 else if (r == 2||r==3) //아래
                 {
                     gold.transform.position = new Vector2(
-                        Random.Range(player.position.x-radMaxX,player.position.x+radMaxX), 
-                        Random.Range(player.position.y-radMinY,player.position.y-radMaxY));
+                        Random.Range(player.position.x-radMaxX*Camera.main.orthographicSize,player.position.x+radMaxX*Camera.main.orthographicSize), 
+                        Random.Range(player.position.y-radMinY*Camera.main.orthographicSize,player.position.y-radMaxY*Camera.main.orthographicSize));
                 }
                 else if (r == 4) //오른쪽
                 {
                     gold.transform.position = new Vector2(
-                        Random.Range(player.position.x + radMinX, player.position.x + radMaxX),
-                        Random.Range(player.position.y - radMinY, player.position.y + radMinY));
+                        Random.Range(player.position.x + radMinX*Camera.main.orthographicSize, player.position.x + radMaxX*Camera.main.orthographicSize),
+                        Random.Range(player.position.y - radMinY*Camera.main.orthographicSize, player.position.y + radMinY*Camera.main.orthographicSize));
                 }
                 else if (r == 5) //왼쪽
                 {
                     gold.transform.position = new Vector2(
-                        Random.Range(player.position.x - radMinX, player.position.x - radMaxX),
-                        Random.Range(player.position.y - radMinY, player.position.y + radMinY));
+                        Random.Range(player.position.x - radMinX*Camera.main.orthographicSize, player.position.x - radMaxX*Camera.main.orthographicSize),
+                        Random.Range(player.position.y - radMinY*Camera.main.orthographicSize, player.position.y + radMinY*Camera.main.orthographicSize));
                 }   
             }
         }
     }
 
+    IEnumerator goldTimeMinus()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeMinusDelay);
+            if (delay >= minGoldTime)
+                delay -= timeMinusValue;
+            else
+                break;
+        }
+        yield break;
+    }
 }
